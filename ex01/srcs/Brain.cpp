aff87977eb3ee18eebea9b1d4694f7a720d2ec40
Hiroaki Sano <hsano@student.42tokyo.jp>
2023-01-31 03:18:59 +0900

    completed ex01

---
+++ ex01/srcs/Brain.cpp
@@ -0,0 +1,40 @@
+#include "Brain.hpp"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+Brain::Brain()
+{
+    cout << "[Brain] default constructor called" << endl;
+}
+
+Brain::Brain(const Brain &brain)
+{
+    cout << "[Brain] Copy constructor called" << endl;
+    for(int i=0; i < array_size_; i++)
+    {
+        this->ideas[i] = brain.ideas[i];
+    }
+}
+
+void Brain::operator=(const Brain &brain)
+{
+    cout << "[Brain]Copy assignment operator called" << endl;
+    for(int i=0; i < array_size_; i++)
+    {
+        this->ideas[i] = brain.ideas[i];
+    }
+}
+
+Brain::~Brain()
+{
+    cout << "[Brain] Destructor called" << endl;
+}
+
+void Brain::deepcopy(const Brain* src, Brain* dst) const
+{
+    for(int i=0; i < array_size_; i++)
+    {
+        dst->ideas[i] = src->ideas[i];
+    }
+}
